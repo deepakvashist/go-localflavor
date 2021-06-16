f0b30d08db30b3d76c08eeb5baf309e0bab2e260
Deepak Vashist <deepaksvashist@gmail.com>
2021-06-15 23:10:46 -0300

    docs: fix br errors godoc

--- br/error.go
+++ br/error.go
@@ -5,8 +5,8 @@ import "errors"
 var (
 	// ErrInvalidDocumentPattern is returned when the document pattern is invalid.
 	ErrInvalidDocumentPattern = errors.New("invalid document patter")
-	// ErrInvalidDocumentPattern is returned when the document number is invalid.
+	// ErrInvalidDocumentNumber is returned when the document number is invalid.
 	ErrInvalidDocumentNumber = errors.New("invalid document number")
-	// ErrInvalidDocumentNumber is returned when the CEP pattern is invalid.
+	// ErrInvalidCEPPattern is returned when the CEP pattern is invalid.
 	ErrInvalidCEPPattern = errors.New("invalid CEP number")
 )
